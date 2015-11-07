c3f598576c7f7c8b04f7edbaf3055df40c776b3a
root <root@Raspyfi>
2015-11-07 10:44:09 +0000

    discovery

---
+++ albumart/ad.js
@@ -0,0 +1,10 @@
+var mdns = require('mdns');
+
+var txt_record = {
+			volumioName: 'opera',
+			UUID: 'adfsda3546y4g'
+		};
+
+// advertise a http server on port 4321
+var ad = mdns.createAdvertisement(mdns.tcp('volumio'),3000, {txtRecord: txt_record});
+ad.start();
