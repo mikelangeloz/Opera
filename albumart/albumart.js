6870cc91b8a9d0bd4fb355d121f8d5cb45f6bd74
Volumio <info@volumio.org>
2015-11-24 21:26:53 +0100

    Update albumart.js

--- albumart/albumart.js
+++ albumart/albumart.js
@@ -7,19 +7,6 @@ var uuid = require('node-uuid');
 var nodetools=require('nodetools');
 
 
-
-//Discovery 
-var mdns = require('mdns');
-
-var txt_record = {
-                        volumioName: 'opera',
-                        UUID: 'adfsda3546y4g'
-                };
-
-// advertise a http server on port 4321
-var ad = mdns.createAdvertisement(mdns.tcp('volumio'),3000, {txtRecord: txt_record});
-ad.start();
-
 var albumArtRootFolder='/data/albumart';
 
 var setFolder=function(newFolder)
